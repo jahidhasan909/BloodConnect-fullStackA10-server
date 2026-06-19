4261735d541b4dde93d741bfe5e94d83a0672e76
jahidhasan909 <jjjjahidhasan999@gmail.com>
2026-06-19 22:51:13 +0600

    post api mathod make and save user data in mongodb successfully

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 8000
@@ -7,6 +8,47 @@ const port = process.env.PORT || 8000
 
 app.use(cors());
 app.use(express.json())
+
+
+
+
+const uri = process.env.MONGODB_URI
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+
+        const database = client.db(process.env.MONGODB_DB)
+        const userCollaction = database.collection('usercollaction')
+
+
+        app.post('/api/users', async (req, res) => {
+            const userdocs = req.body
+            const result = await userCollaction.insertOne(userdocs)
+            res.json(result)
+        })
+
+
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
